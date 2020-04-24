cbeadc3f94e8f71656703d0be80e121f5c5f70ac
Veleblazevski <blazhevski.vele@gmail.com>
2020-04-24 15:21:36 +0200

    client initial commit

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,30 @@
+import {
+    SET_CURRENT_USER,
+    USER_LOADING
+  } from "../actions/types";
+
+  const isEmpty = require('is-empty');
+
+  const initialState = {
+      isAuthenticated: false,
+      user: {},
+      loading:false
+  };
+
+  export default function(state = initialState,actions) {
+      switch(actions.type) {
+          case SET_CURRENT_USER: 
+          return {
+              ...state,
+              isAuthenticated: !isEmpty(actions.payload),
+              user:actions.payload
+          };
+          case USER_LOADING: 
+          return {
+              ...state,
+              loading:true
+          };
+          default: 
+          return state;
+      }
+  }
